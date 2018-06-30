bc3717ce76f5be9323fa1febfa731ac9ddea96f4
Indesignify <Indesignify@gmail.com>
2018-06-30 23:29:43 +0300

    Some cleanup done.

--- src/main/java/ru/spb/iac/repository/hibernate/UseIdOrGenerate.java
+++ src/main/java/ru/spb/iac/repository/hibernate/UseIdOrGenerate.java
@@ -13,21 +13,12 @@ public class UseIdOrGenerate extends IdentityGenerator {
     public Serializable generate(SharedSessionContractImplementor session, Object obj) throws HibernateException {
         if (obj == null) throw new HibernateException(new NullPointerException()) ;
 
-        if ((((EntityWithId) obj).getId()) == null) {//id is null it means generate ID
+        if ((((EntityWithId) obj).getId()) == null) {
             Serializable id = super.generate(session, obj) ;
             return id;
         } else {
-            return ((EntityWithId) obj).getId();//id is not null so using assigned id.
+            return ((EntityWithId) obj).getId();
 
         }
     }
 }
-
-//public class UseIdOrGenerate extends IdentityGenerator {
-//
-//    @Override
-//    public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
-//        Serializable id = session.getEntityPersister(null, object).getClassMetadata().getIdentifier(object, session);
-//        return id != null ? id : super.generate(session, object);
-//    }
-//}
