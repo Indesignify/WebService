ff708200f23e9aee0e9a7dd043ecc9fac495bd99
Indesignify <Indesignify@gmail.com>
2018-07-02 12:35:43 +0300

    Thymeleaf and Spring Security dependencies added. Spring configuration added, authentication done!

--- src/main/java/ru/spb/iac/controller/PersonController.java
+++ src/main/java/ru/spb/iac/controller/PersonController.java
@@ -1,5 +1,9 @@
 package ru.spb.iac.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +12,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PersonController {
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
     public ModelAndView startingPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView adminLogin() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("persons");
+        return modelAndView;
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public ModelAndView personsPage() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("persons");
         return modelAndView;
